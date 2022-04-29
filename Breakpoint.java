6e675369aae31e620391bd5fe0f48f923c4dacc6
William <79172861+wuhoo123@users.noreply.github.com>
2022-04-29 09:08:09 -0400

    Update Breakpoint.java

--- Breakpoint.java
+++ Breakpoint.java
@@ -1,9 +1,11 @@
+/** 
+ * Welcome to Ram's Amazing program.
+ * */  
 public class Breakpoint {
     public static void main(String [] args){
         int a = 1000;
         int b = 1;
-        while (a > 1)
-        {
+        while (a > 1){
             a /= b;
             b += 1;
             System.out.println(a);
